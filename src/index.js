02a83d7b7dabd91a674de3a9eddce9a68b22e8bd
Alex <49212602+prikhodkoas@users.noreply.github.com>
2022-01-31 17:12:55 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrayForCheck = str.split('');
+
+    let arrayTest = [];
+
+    let res;
+    for(let i = 0; i < arrayForCheck.length; i++) {
+        res = false;
+
+        for(let row = 0; row < bracketsConfig.length; row++) {
+            if(bracketsConfig[row][0] === bracketsConfig[row][1]) {
+                if(arrayForCheck[i] === bracketsConfig[row][0]) {
+                    if(arrayTest[arrayTest.length - 1] === bracketsConfig[row][0]) {
+                        arrayTest.pop();
+                        res = true;
+                        break;
+                    }
+                    else {
+                        arrayTest.push(arrayForCheck[i]);
+                        res = true;
+                        break;
+                    }
+                }
+            }
+
+            if(arrayForCheck[i] === bracketsConfig[row][0]) {
+                arrayTest.push(arrayForCheck[i]);
+                res = true;
+                break;
+            }
+
+            if(arrayForCheck[i] === bracketsConfig[row][1]) {
+                if(arrayTest.pop() !== bracketsConfig[row][0]) {
+                    return false;
+                }
+                else {
+                    res = true;
+                    break;
+                }
+            }
+        }
+
+        if(res === false) {
+            return false;
+        }
+    }
+
+    if(arrayTest.length > 0) {
+        return false;
+    }
+
+    return true;
 }
